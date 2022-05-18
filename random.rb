4cda30f0bde7e256fec9089da43d921fb1c16bb8
koki <cajon.pj09@gmail.com>
2022-05-18 20:10:50 +0900

    2回目の提出

--- random.rb
+++ random.rb
@@ -1,24 +1,9 @@
-members = ["A","B","C","D","E","F"]
-
-# 3、3に分割
-split_3_a_group = members.sample(3).sort
-split_3_b_group = members - split_3_a_group.sort
-
-# 2、4に分割
-split_24_a_group =  members.sample(2).sort
-split_24_b_group = members - split_24_a_group.sort
-
-
-pattern = rand(2)
-
-
-if pattern == 0
-  p split_3_a_group
-  p split_3_b_group
-elsif pattern == 1
-  p split_24_a_group
-  p split_24_b_group
+def split_group
+  members = ["A","B","C","D","E","F"]
+  split_group = members.sample(rand(2..3)).sort
+  p split_group
+  p members - split_group
 end
 
-  
+split_group
 

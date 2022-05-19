5c3087c81371077e8c85aee5bb4f6f0bf8209291
koki <cajon.pj09@gmail.com>
2022-05-19 22:15:44 +0900

    変数名修正

--- random.rb
+++ random.rb
@@ -1,9 +1,9 @@
-def splitted_group
+def divide_group
   members = ["A","B","C","D","E","F"]
-  split_group = members.sample(rand(2..3)).sort
-  p split_group
-  p members - split_group
+  first_group = members.sample(rand(2..3)).sort
+  p first_group
+  p members - first_group
 end
-
-splitted_group
+ 
+divide_group
 

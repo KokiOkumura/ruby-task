f51be36a86c0a2dd8c369d4a2be54be4027fc5f6
koki <cajon.pj09@gmail.com>
2022-06-01 22:53:40 +0900

    カレンダー課題

--- random.rb
+++ random.rb
@@ -1,9 +1,8 @@
 def divide_group
-  members = ["A","B","C","D","E","F"]
+  members = %w[A B C D E F]
   first_group = members.sample(rand(2..3)).sort
   p first_group
   p members - first_group
 end
- 
-divide_group
 
+divide_group

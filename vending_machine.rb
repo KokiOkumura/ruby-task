967a255f5099e865f78cd7b58a14f313b9784a4e
koki <cajon.pj09@gmail.com>
2022-06-11 20:03:11 +0900

    自動販売機修正

--- vending_machine.rb
+++ vending_machine.rb
@@ -23,18 +23,15 @@ end
 class Vending_Machine
   attr_accessor :sales
 
+  DRINK_LIST = []
+
   def initialize(sales = 0)
     @sales = sales
   end
 
   def in_stock(drink, stock)
     drink.stock += stock
-  end
-
-  def drink_info(drink)
-    p drink.name
-    p drink.price
-    p drink.stock
+    DRINK_LIST.push(drink)
   end
 
   def to_buy(suica, drink)
@@ -50,15 +47,19 @@ suica = Suica.new(500)
 suica.charge_amount(1000)
 suica.charge_amount(1000)
 
-drink = Drink.new(name: 'cola', price: 120)
-drink2 = Drink.new(name: 'redbull', price: 200)
+cola = Drink.new(name: 'cola', price: 120)
 
 vending_machine = Vending_Machine.new
-vending_machine.in_stock(drink, 5)
-vending_machine.in_stock(drink, 3)
-vending_machine.to_buy(suica, drink)
-vending_machine.drink_info(drink)
-vending_machine.sales
+vending_machine.in_stock(cola, 5)
+vending_machine.in_stock(cola, 3)
 
-# vending_machine.in_stock(drink2, 5)
-# vending_machine.drink_info(drink2, 5)
+vending_machine.to_buy(suica, cola)
+
+redbull = Drink.new(name: 'redbull', price: 200)
+vending_machine.in_stock(redbull, 5)
+water = Drink.new(name: 'water', price: 100)
+vending_machine.in_stock(water, 5)
+
+vending_machine.to_buy(suica, redbull)
+
+p Vending_Machine::DRINK_LIST
--- vending_machinecopy.rb
+++ vending_machinecopy.rb
@@ -6,19 +6,17 @@ class Suica
   end
 
   def charge_amount(money)
-    if money >= 100
-      @balance += money
-    end
+    @balance += money if money >= 100
   end
 end
 
 class Drink
   attr_accessor :name, :price, :stock
 
-  def initialize(name:, price:)
+  def initialize(name: 'cola', price: 120, stock: 5)
     @name = name
     @price = price
-    @stock = 0
+    @stock = stock
   end
 end
 
@@ -31,13 +29,12 @@ class Vending_Machine
 
   def in_stock(drink, stock)
     drink.stock += stock
+    @drink_list = { name: drink.name, price: drink.price, stock: drink.stock }
   end
 
-  # def self.drink_info(drinks)
-  #   drinks.map do |drink|
-  #     drink.name
-  #   end
-  # end
+  def drink_info
+    p @drink_list
+  end
 
   def to_buy(suica, drink)
     if drink.stock > 1 && drink.price <= suica.balance
@@ -50,16 +47,22 @@ end
 
 suica = Suica.new(500)
 suica.charge_amount(1000)
-p suica.charge_amount(1000)
+suica.charge_amount(1000)
 
-drink = Drink.new(name: 'cola', price: 120)
-drink2 = Drink.new(name: 'redbull', price: 200)
+drink = Drink.new
 
 vending_machine = Vending_Machine.new
-vending_machine.in_stock(drink, 5)
-vending_machine.in_stock(drink, 3)
-vending_machine.to_buy(suica, drink)
-# Vending_machine.drink_info(drinks)
-# vending_machine.sales
+vending_machine.in_stock(drink)
+# vending_machine.in_stock(cola, 5)
+# vending_machine.in_stock(cola, 3)
 
+# vending_machine.to_buy(suica, cola)
 
+# redbull = Drink.new(name: 'redbull', price: 200)
+# vending_machine.in_stock(redbull, 5)
+# water = Drink.new(name: 'water', price: 100)
+# vending_machine.in_stock(water, 5)
+
+# vending_machine.to_buy(suica, redbull)
+
+# vending_machine.drink_info

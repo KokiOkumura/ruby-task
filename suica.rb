ede7761d74f75e587eadcc35c077f02043dec91c
koki <cajon.pj09@gmail.com>
2022-06-17 21:50:29 +0900

    自動販売機課題修正2022_06_17

---
+++ suica.rb
@@ -0,0 +1,14 @@
+class Suica
+  attr_accessor :balance
+  attr_reader :age, :sex
+
+  def initialize(age, sex, balance = 0)
+    @age = age
+    @sex = sex
+    @balance = balance
+  end
+
+  def charge_amount(money)
+    @balance += money if money >= 100
+  end
+end
